188b4ee914b1ad9746adbbbda4877d9a5c50556f
opilane <opilane@HP-CZC0359NP3>
2025-10-15 12:57:10 +0300

    Test Commit

--- StoredProcedureTARpe24/Controllers/EmployeeController.cs
+++ StoredProcedureTARpe24/Controllers/EmployeeController.cs
@@ -133,5 +133,33 @@ namespace StoredProcedureTARpe24.Controllers
 
             }
         }
+
+        [HttpGet]
+        public IActionResult Test()
+        {
+
+            string connectionString = _config.GetConnectionString("DefaultConnection");
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "dbo.spSearchEmployees";
+                cmd.CommandType = System.Data.CommandType.Text;
+                con.Open();
+                SqlDataReader sdr = cmd.ExecuteReader();
+                List<Employee> model = new List<Employee>();
+                while (sdr.Read())
+                {
+                    var details = new Employee();
+                    details.FirstName = sdr["FirstName"].ToString();
+                    details.LastName = sdr["LastName"].ToString();
+                    details.Gender = sdr["Gender"].ToString();
+                    details.Salary = Convert.ToInt32(sdr["Salary"]);
+                    model.Add(details);
+                }
+                return View(model);
+
+            }
+        }
     }
 }
